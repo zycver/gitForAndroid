ea5cf41f65338439a32eef0c1f00a43969cc3e83
zycver <919127869@qq.com>
2017-05-17 10:27:03 +0800

    [ 提交第一个版本]

---
+++ app/src/main/java/com/example/zhouyucheng/pplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhouyucheng.pplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
